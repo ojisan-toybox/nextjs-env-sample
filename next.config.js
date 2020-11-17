9766033752c72975c9c6f631360fba4b9c422841
sadness_ojisan <sadness.ojisan@gmail.com>
2020-11-18 03:55:26 +0900

    NODE_ENVはランタイムでは本来の値に書き換えられる

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 module.exports = {};
 
 // console.log("<next.config.js> process.env:", process.env);
-console.log("<next.config.js> process.env:", process.env.NODE_ENV);
+console.log("<next.config.js> process.env.NODE_ENV:", process.env.NODE_ENV);
+console.log(
+  "<next.config.js> process.env.ORIGINAL_ENV: ",
+  process.env.ORIGINAL_ENV
+);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,20 @@
 export default () => {
+  console.log("<component> process.env.NODE_ENV: ", process.env.NODE_ENV);
+  console.log(
+    "<component> process.env.ORIGINAL_ENV: ",
+    process.env.ORIGINAL_ENV
+  );
   return <div>hello world!!</div>;
+};
+
+export const getServerSideProps = () => {
+  console.log(
+    "<getServerSideProps> process.env.NODE_ENV: ",
+    process.env.NODE_ENV
+  );
+  console.log(
+    "<getServerSideProps> process.env.ORIGINAL_ENV: ",
+    process.env.ORIGINAL_ENV
+  );
+  return {};
 };
